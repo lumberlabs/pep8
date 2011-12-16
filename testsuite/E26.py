8bf7df03f88f121fb44f6a0e7d333ba31d3ee58c
Josh Bleecher Snyder <josharian+github@gmail.com>
2011-12-15 17:06:38 -0800

    Add blank line test case

--- testsuite/E26.py
+++ testsuite/E26.py
@@ -3,6 +3,10 @@ pass # an inline comment
 #: E261
 x = (x + 1 +  # Increment x
     1) # twice!
+#: E261
+x = (x + 1 +  # Increment x
+
+    1) # twice!
 #: E262
 x = x + 1  #Increment x
 #: E262
@@ -12,4 +16,8 @@ x = (x + 1 +  # Increment x
     1)        #  twice!
 #: E262
 x = (x + 1 +  # Increment x
+    1)        #twice!
+#: E262
+x = (x + 1 +  # Increment x
+
     1)        #twice!
