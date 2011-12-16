ceca755a5b67ec6743533c8a95c74ab3cd656a0c
Josh Bleecher Snyder <josharian+github@gmail.com>
2011-12-15 16:26:31 -0800

    Add multi-line tests for whitespace_before_inline_comment

--- testsuite/E26.py
+++ testsuite/E26.py
@@ -1,6 +1,15 @@
 #: E261
 pass # an inline comment
+#: E261
+x = (x + 1 +  # Increment x
+    1) # twice!
 #: E262
 x = x + 1  #Increment x
 #: E262
 x = x + 1  #  Increment x
+#: E262
+x = (x + 1 +  # Increment x
+    1)        #  twice!
+#: E262
+x = (x + 1 +  # Increment x
+    1)        #twice!
